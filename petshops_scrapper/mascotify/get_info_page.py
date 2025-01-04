160be6dc3b82fbee4d07f17873ad89465015bf43
jhon <fr.jhonk@gmail.com>
2025-01-03 23:33:06 -0500

    mascotify scrapper

--- petshops_scrapper/mascotify/get_info_page.py
+++ petshops_scrapper/mascotify/get_info_page.py
@@ -1,62 +1,73 @@
-import requests, json, re
+import os
+import json
+import re
+import requests
+import pandas as pd
 from bs4 import BeautifulSoup
-from rich import print
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
 def clean_text(text: str) -> str:
-    """Elimina espacios innecesarios en un texto."""
+    "Elimina espacios innecesarios."
     text = re.sub(r"\s+", " ", text)
     return text.strip()
 
 def clean_numbers(text: str) -> float:
-    """Extrae el primer número de un texto como flotante."""
+    "Extrae el primer número como flotante."
     match = re.search(r"\d+(\.\d+)?", text)
     if match:
         return float(match.group())
     return None
 
 def get_soup(url: str) -> BeautifulSoup:
-    """Obtiene el contenido HTML de una URL y lo convierte en un objeto BeautifulSoup."""
+    "Obtiene el HTML de una URL."
     html = requests.get(url).text
     return BeautifulSoup(html, "html.parser")
 
-class MascotifyScraper:
-    """Clase para extraer información de productos desde una página web."""
+class ProductScraper:
+    "Extrae información de productos."
 
     def __init__(self, url: str):
         self.url = url
         self.soup = get_soup(url)
 
-    def find_basic_elements(self) -> dict:
-        """Extrae los elementos básicos de un producto."""
-        category_nav = self.soup.find('nav', id = 'breadcrumbs')
-        if category_nav:
-            categories = category_nav.find_all('a')
-            categories = [cat.get_text() for cat in categories]
-            menu = categories[1]
-            category = " - ".join(categories[2:])
+    def scrape_product(self) -> dict:
+        "Extrae los detalles de un producto."
+        try:
+            category_nav = self.soup.find("nav", id="breadcrumbs")
+            if category_nav:
+                categories = category_nav.find_all("a")
+                categories = [cat.get_text() for cat in categories]
+                menu = categories[1]
+                category = " - ".join(categories[2:])
+            else:
+                menu, category = None, None
 
-        h1 = self.soup.find("h1", class_="product-title")
-        brand = h1.find_next("div", class_="marca-product-page").find_next("div").get_text()
-        short_description = h1.find_next(
-            "div", class_="product-short-description"
-        ).get_text()
-        long_description_divs = self.soup.find('div', class_='panel entry-content')
+            h1 = self.soup.find("h1", class_="product-title")
+            brand = (
+                h1.find_next("div", class_="marca-product-page")
+                .find_next("div")
+                .get_text()
+            )
+            short_description = h1.find_next(
+                "div", class_="product-short-description"
+            ).get_text()
+            long_description_divs = self.soup.find("div", class_="panel entry-content")
 
-        last_price = h1.find_next("del")
-        actual_price = h1.find_next("ins")
+            last_price = h1.find_next("del")
+            actual_price = h1.find_next("ins")
 
-        price_min, price_max, qnts, prices = None, None, [], []
-        prices_str, qnts_str = None, None
+            price_min, price_max, qnts, prices = None, None, [], []
+            prices_str, qnts_str = None, None
 
-        if last_price:
-            last_price = clean_numbers(last_price.get_text())
+            if last_price:
+                last_price = clean_numbers(last_price.get_text())
 
-        if actual_price:
-            actual_price = clean_numbers(actual_price.get_text())
+            if actual_price:
+                actual_price = clean_numbers(actual_price.get_text())
 
-        variation_data = self.soup.find("form", class_="variations_form")
-        if variation_data:
-            try:
+            variation_data = self.soup.find("form", class_="variations_form")
+            if variation_data:
                 variation_json = variation_data.get("data-product_variations")
                 variations = json.loads(variation_json)
 
@@ -68,31 +79,109 @@ class MascotifyScraper:
                 price_min = min(prices)
                 price_max = max(prices)
 
-                prices_str = " - ".join([str(x) for x in prices])
+                prices_str = " - ".join(map(str, prices))
                 qnts_str = " - ".join(qnts)
+
+            return {
+                "menu": menu,
+                "category": category,
+                "name": clean_text(h1.get_text()),
+                "brand": clean_text(brand),
+                "price": actual_price,
+                "last_precio": last_price,
+                "price_min": price_min,
+                "price_max": price_max,
+                "range_prices": prices_str,
+                "range_qnts": qnts_str,
+                "s_description": clean_text(short_description),
+                "l_description": clean_text(
+                    long_description_divs.get_text() if long_description_divs else ""
+                ),
+                "url": self.url,
+            }
+        except Exception:
+            return {}
+
+class MascotifyDataProcessor:
+    "Procesa productos y guarda en Excel."
+
+    def __init__(self, last_json_path, output_path, max_workers=4):
+        self.last_json_path = last_json_path
+        self.output_path = output_path
+        self.max_workers = max_workers
+        self.product_urls = []
+
+    def load_product_urls(self, filtro_text=None, filtro_tipo=None):
+        "Carga las URLs de productos con filtros opcionales."
+        with open(self.last_json_path, "r") as file:
+            data = json.load(file)
+
+        for category in data:
+            if filtro_text and filtro_text not in category["text"].lower():
+                continue
+
+            for product in category.get("products", []):
+                if filtro_tipo and filtro_tipo not in product.lower():
+                    continue
+                self.product_urls.append(product)
+        print(f"Total URLs cargadas: {len(self.product_urls)}")
+
+    def process_products(self):
+        "Procesa los productos y guarda los resultados."
+        results = []
+        
+        
+        pbar = tqdm(total=len(self.product_urls), desc="Procesando productos")
+        
+        def process_url(url):
+            try:
+                result = ProductScraper(url).scrape_product()
+                pbar.update(1)  #
+                if result:
+                    return result
             except Exception as e:
-                print(f"Error parsing variations: {e}")
+                print(f"Error procesando {url}: {str(e)}")
+                pbar.update(1)  
+                return None
 
-        return {
-            "menu": menu,
-            "category": category,
-            "name": clean_text(h1.get_text()),
-            "brand": clean_text(brand),
-            "precio": actual_price,
-            "last_precio": last_price,
-            "price_min": price_min,
-            "price_max": price_max,
-            "range_prices": prices_str,
-            "range_qnts": qnts_str,
-            "s_description": clean_text(short_description),
-            "l_description": clean_text(long_description_divs.get_text() if long_description_divs else "")
-        }
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            
+            futures = list(executor.map(process_url, self.product_urls))
+            
+            
+            results = [r for r in futures if r]
 
-# Ejemplo de uso
-url = "https://mascotify.pe/gatos/accesorios-y-otros-gatos/juguetes-gatos/catit-groovy-fish-juguete-para-gatos/"
-url = 'https://mascotify.pe/gatos/alimento-gatos/leches-y-formulas-gatos/petag-kmr-sustituto-de-leche-en-polvo-para-gatitos/'
-url = 'https://mascotify.pe/otros/conejos/brit-animals-rabbit-adult-complete/'
-url = 'https://mascotify.pe/perros/accesorios-y-otros/platos-bebederos/ferplast-contenedor-feedy-para-comida-seca/'
-scraper = MascotifyScraper(url)
-data = scraper.find_basic_elements()
-print(data)
+        pbar.close()  
+
+        df = pd.DataFrame(results)
+        df.rename(
+            columns={
+                "menu": "Menu",
+                "category": "Categoria",
+                "name": "Nombre",
+                "brand": "Marca",
+                "price": "Precio",
+                "last_precio": "Precio Anterior (Descuento)",
+                "price_min": "Precio Minimo por Pequeña Cantidad",
+                "price_max": "Precio Maximo por Grande Cantidad",
+                "range_prices": "Precios para Cada Cantidad",
+                "range_qnts": "Cantidades Disponibles",
+                "s_description": "Descripcion Corta",
+                "l_description": "Descripcion Larga",
+                "url": "URL",
+            },
+            inplace=True,
+        )
+
+        os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
+        df.to_excel(self.output_path, index=False)
+
+if __name__ == "__main__":
+    processor = MascotifyDataProcessor(
+        last_json_path="info/last/mascotify.json",
+        output_path="raw_data/mascotify.xlsx",
+        max_workers=30,
+    )
+
+    processor.load_product_urls()
+    processor.process_products()
--- petshops_scrapper/mascotify/get_menus.py
+++ petshops_scrapper/mascotify/get_menus.py
@@ -1,84 +1,128 @@
+import os
+import json
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlsplit
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 from rich import print
 
-url = "https://mascotify.pe/"
-superpet_parse = urlsplit(url)
-html = requests.get(url).content
 
-soup = BeautifulSoup(html, "html.parser")
+class MascotifyScraper:
+    def __init__(self, base_url, max_workers=4):
+        self.base_url = base_url
+        self.superpet_parse = urlsplit(base_url)
+        self.relevant_menu = []
+        self.max_workers = max_workers  # Número de hilos para procesamiento en paralelo
 
+    @staticmethod
+    def get_soup(url: str) -> BeautifulSoup:
+        """Obtiene el contenido HTML de una URL y lo convierte en un objeto BeautifulSoup."""
+        html = requests.get(url).text
+        return BeautifulSoup(html, "html.parser")
 
-mega_menu = soup.find("ul", id="mega-menu-primary")
+    def collect_relevant_menu(self):
+        """Recolecta las categorías relevantes del menú."""
+        html = requests.get(self.base_url).content
+        soup = BeautifulSoup(html, "html.parser")
+        mega_menu = soup.find("ul", id="mega-menu-primary")
 
+        hrefs = [
+            {"href": div.get("href", ""), "text": div.get_text()}
+            for div in mega_menu.find_all("a", class_="mega-menu-link", href=True)
+        ]
 
-hrefs = [
-    {"href": div.get("href", "")}
-    for div in mega_menu.find_all("a", class_="mega-menu-link", href=True)
-]
+        for menu_href in hrefs:
+            _url_split = urlsplit(menu_href.get("href"))
+            _path = _url_split.path.split("/")
+            if _url_split.netloc == self.superpet_parse.netloc and len(_path) > 2:
+                if _path[2] != "":
+                    menu_href["tipo"] = _path[1]
+                    self.relevant_menu.append(menu_href)
 
+    @staticmethod
+    def products_first_page(url):
+        """Obtiene productos y enlaces de páginas adicionales desde la primera página."""
+        soup_page = MascotifyScraper.get_soup(url)
 
-def get_soup(url: str) -> BeautifulSoup:
-    """Obtiene el contenido HTML de una URL y lo convierte en un objeto BeautifulSoup."""
-    html = requests.get(url).text
-    return BeautifulSoup(html, "html.parser")
-
-
-relevant_menu = []
-for menu_href in hrefs:
-    _url_split = urlsplit(menu_href.get("href"))
-    _path = _url_split.path.split("/")
-    if _url_split.netloc == superpet_parse.netloc and len(_path) > 2:
-        if _path[2] != "":
-            menu_href["tipo"] = _path[1]
-            relevant_menu.append(menu_href)
-
-# print(relevant_menu)
-url_sample = "https://mascotify.pe/gatos/accesorios-y-otros-gatos/"
-# url_sample = 'https://mascotify.pe/otros/aves/'
-
-N = 20
-
-
-def products_first_page(url):
-    soup_page = get_soup(url)
-
-    pages_a = soup_page.find("ul", class_="page-numbers")
-    pages_links = []
-    if pages_a:
-        pages_a = pages_a.find_all("a", href=True, class_="page-number")
+        pages_a = soup_page.find("ul", class_="page-numbers")
         pages_links = []
-        for page in pages_a:
-            if page.get("href") not in pages_links:
-                pages_links.append(page.get("href"))
-    return soup_page, pages_links
+        if pages_a:
+            pages_a = pages_a.find_all("a", href=True, class_="page-number")
+            for page in pages_a:
+                if page.get("href") not in pages_links:
+                    pages_links.append(page.get("href"))
+        return soup_page, pages_links
 
+    @staticmethod
+    def get_products_links(soup_page) -> list:
+        """Obtiene los enlaces de productos de una página."""
+        container_products = soup_page.find("div", class_="products")
+        if not container_products:
+            return []
+        products = container_products.find_all("div", class_="image-zoom")
+        return [
+            product.find("a", href=True).get("href", "")
+            for product in products
+            if product.find("a", href=True)
+        ]
 
-def get_products_links(soup_page) -> list:
-    container_products = soup_page.find("div", class_="products")
-    products = container_products.find_all("div", class_="image-zoom")
-    anclas_products = [
-        product.find("a", href=True).get("href", "") for product in products
-    ]
-    # retornar los productos, more_products?, total_pages
-    return anclas_products
+    def get_all_data_products_in_category(self, url):
+        """Obtiene todos los productos de una categoría."""
+        soup_f_page, links = self.products_first_page(url)
+        products = self.get_products_links(soup_f_page)
 
+        if len(links) == 0:
+            return products
 
-def get_all_data_products_in_category(url):
-
-    soup_f_page, links = products_first_page(url)
-    products = get_products_links(soup_f_page)
-    if len(links) == 0:
+        for page_link in links:
+            soup_n_page = self.get_soup(page_link)
+            products_n = self.get_products_links(soup_n_page)
+            products.extend(products_n)
         return products
 
-    for page_link in links:
+    def fetch_menu_products(self):
+        """Asocia los productos con cada categoría en paralelo y muestra progreso."""
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            futures = {
+                executor.submit(
+                    self.get_all_data_products_in_category, menu["href"]
+                ): menu
+                for menu in self.relevant_menu
+            }
 
-        soup_n_page = get_soup (page_link)
-        products_n = get_products_links(soup_n_page)
-        products.extend(products_n)
-    return products
+            for future in tqdm(
+                as_completed(futures), total=len(futures), desc="Procesando categorías"
+            ):
+                menu = futures[future]
+                try:
+                    menu["products"] = future.result()
+                except Exception as e:
+                    print(f"[red]Error procesando {menu['href']}: {e}[/red]")
+
+    def save_data(self):
+        """Guarda los datos en los archivos JSON requeridos."""
+        if not os.path.exists("info/last"):
+            os.makedirs("info/last")
+        if not os.path.exists("info/history/mascotify"):
+            os.makedirs("info/history/mascotify")
+
+        # Guardar en info/last/mascotify.json
+        with open("info/last/mascotify.json", "w") as last_file:
+            json.dump(self.relevant_menu, last_file, indent=4)
+
+        # Guardar en info/history/mascotify/Y-M-d.json
+        today = datetime.now().strftime("%Y-%m-%d")
+        history_path = f"info/history/mascotify/{today}.json"
+        with open(history_path, "w") as history_file:
+            json.dump(self.relevant_menu, history_file, indent=4)
 
 
-data = get_all_data_products_in_category('https://mascotify.pe/gatos/accesorios-y-otros-gatos/?swoof=1&product_cat=transportadores-gatos&really_curr_tax=82-product_cat')
-print(data)
+# Uso de la clase
+if __name__ == "__main__":
+    scraper = MascotifyScraper("https://mascotify.pe/", max_workers=5)
+    scraper.collect_relevant_menu()
+    scraper.fetch_menu_products()
+    scraper.save_data()
+    print("[green]Datos recolectados y guardados exitosamente.[/green]")
