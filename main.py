9f335fe9d18f4a616beb16a7586bc651deb6070a
jhon <fr.jhonk@gmail.com>
2025-01-05 11:55:29 -0500

    run locally

--- main.py
+++ main.py
@@ -9,27 +9,36 @@ from petshops_scrapper.mascota_veloz import MascotaVelozMenu
 from petshops_scrapper.superpet import SuperPetMenu
 from rich import print
 
-# print("[orange3]Obteniendo Menus de Todas las paginas[/orange3]")
-# SuperPetMenu()
-# MascotifyMenu()
-# MisterPetMenu()
-# MascotaVelozMenu()
-# PharmivetMenu()
+print("[orange3]Obteniendo Menus de Todas las paginas[/orange3]")
+SuperPetMenu()
+MascotifyMenu()
+MisterPetMenu()
+MascotaVelozMenu()
+PharmivetMenu()
 
 
-os.makedirs('raw_data/', exist_ok=True)
+os.makedirs("raw_data/", exist_ok=True)
+
+from petshops_scrapper import MascotaVeloz, Mascotify, MisterPet, Pharmivet, SuperPet
 
 RUN_LOCAL_EXISTING_PRODUCTS = True
 TOTAL_WORKERS = 30
 
-m1 = MascotaVeloz(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
-m2 = Mascotify   (local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
-m3 = MisterPet   (local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
-m4 = Pharmivet   (local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
-m5 = SuperPet    (local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
-# print(data_i)
-m1.fetch_all_information()
-m2.fetch_all_information()
-m3.fetch_all_information()
-m4.fetch_all_information()
-m5.fetch_all_information()
+MascotaVeloz(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
+Mascotify(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
+MisterPet(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
+Pharmivet(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
+SuperPet(local=RUN_LOCAL_EXISTING_PRODUCTS, n_workers=TOTAL_WORKERS)
+
+
+from petshops_scrapper import NAMES, DataProcessor
+
+WORKERS_PER_COMPANY = {
+    "SuperPet": 20,
+    "MascotaVeloz": 10,
+    "Mascotify": 3,
+    "PharmiVet": 5,
+    "MisterPet": 5,
+}
+
+data = DataProcessor(workers_per_company=WORKERS_PER_COMPANY, names=NAMES).process_all()
